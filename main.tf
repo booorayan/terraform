5ae9368a4814427d0567e1af2e56cbfaa2ce035c
booorayan <boorayo@yahoo.com>
2023-02-09 12:31:01 +0300

    Initial commit

---
+++ main.tf
@@ -0,0 +1,28 @@
+
+resource "vultr_instance" "test_vm" {
+  plan   = var.plan
+  region = var.region
+  os_id  = var.os_id
+  enable_ipv6 = var.enable_ipv6
+  label = "test_vm"
+  firewall_group_id = vultr_firewall_group.test_firewall_group.id
+}
+
+resource "vultr_block_storage" "test_blockstorage" {
+  region  = var.region
+  size_gb = 10
+  attached_to_instance = vultr_instance.test_vm.id
+}
+
+resource "vultr_firewall_group" "test_firewall_group" {
+  description = "Test firewall group"
+}
+
+resource "vultr_firewall_rule" "test_firewall" {
+  firewall_group_id = vultr_firewall_group.test_firewall_group.id
+  ip_type           = "v4"
+  protocol          = "tcp"
+  subnet            = "0.0.0.0"
+  subnet_size       = 0
+  port              = "8000"
+}
---
+++ variable.tf
@@ -0,0 +1,28 @@
+variable "vultr_api_key" {
+  description = "API key for vultr access"
+  type = string
+
+}
+
+variable "plan" {
+  description = "VM Instance plan"
+  type = string
+  default = "vc2-2c-4gb"
+}
+variable "region" {
+  description = "Vultr Region"
+  type = string
+  default = "nrt"
+}
+
+variable "os_id" {
+  description = "Instance OS id"
+  default = "1946"
+}
+
+variable "enable_ipv6" {
+  description = "Enable ipv6 for instance"
+  type = bool
+  default = true
+}
+
